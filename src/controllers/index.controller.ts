9e2a75a0d73d3416d5fd6bb14501bd962b828589
nicolas-rm <65315133+nicolas-rm@users.noreply.github.com>
2020-09-30 17:32:35 -0500

    Configuraciones para pasarela de pago

---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import pool from '../database/conexion.database';
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51HWnnrARF0Uplu4WQn2lBBUJrVjMIhY06hbqCHjj32jDfSN8cm7896RazgIlc03INEgDkhiK2Fb7vIpWON7sNm3W00yZInEgn3', { apiVersion: '2020-08-27' });
+class IndexController {
+
+    // public pay: Stripe;
+
+    constructor() {
+
+    }
+
+
+
+    public async paymethod(req: Request, res: Response): Promise<void> {
+
+        const { id, amount } = req.body;
+
+        console.log(id);
+        console.log(amount);
+        try {
+            const paymenthod = await stripe.paymentIntents.create({
+                amount,
+                currency: 'USD',
+                description: 'Compútador Portatil',
+                payment_method: id,
+                confirm: true
+            });
+            res.status(200).json({
+                OK: true,
+                POST: `Las Credenciales Son Correctas.`,
+                paymenthod
+            });
+        } catch (error) {
+            res.status(400).json({
+                OK: false,
+                POST: `Las Credenciales No Son Correctas.`,
+                error
+            });
+        }
+
+    }
+}
+
+const indexController = new IndexController();
+export default indexController;
